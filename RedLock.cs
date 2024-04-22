67f60da75635baa7846a92e447dd69570a97a5d3
Dolev Raziel <Dolev.Raziel@gmail.com>
2024-04-22 14:46:57 +0300

    added basic redlock class

---
+++ RedLock.cs
@@ -0,0 +1,37 @@
+﻿
+namespace DRedis;
+
+public class RedLock: IRedLock, IDisposable, IAsyncDisposable
+{
+    private readonly string _key;
+    private readonly string _value;
+
+    public bool IsAcquired {get;set;}
+
+    public RedLock(string key, string value)
+    {
+        _key = key;
+        _value = value;
+    }
+
+    public void Release() 
+    {
+        if (!IsAcquired) return;
+        throw new NotImplementedException();    
+    }
+
+    public Task ReleaseAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dispose() 
+    {
+        Release();        
+    }
+
+    public async ValueTask DisposeAsync() 
+    {
+        await ReleaseAsync();
+    }
+}
